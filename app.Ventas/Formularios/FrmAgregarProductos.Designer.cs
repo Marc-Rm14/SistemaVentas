04ae85d4b0439c60af37f807ffd21b02c1ba57bd
Re-L92 <rl4188193@gmail.com>
2025-10-24 13:01:13 -0600

    style: agrega animaciones y bordes redondeados al formulario

--- app.Ventas/Formularios/FrmAgregarProductos.Designer.cs
+++ app.Ventas/Formularios/FrmAgregarProductos.Designer.cs
@@ -46,6 +46,8 @@
             this.txtId = new System.Windows.Forms.TextBox();
             this.lblTitulo = new System.Windows.Forms.Label();
             this.errorIcono = new System.Windows.Forms.ErrorProvider(this.components);
+            this.cuiFormAnimator1 = new CuoreUI.Components.cuiFormAnimator(this.components);
+            this.cuiFormRounder1 = new CuoreUI.Components.cuiFormRounder();
             this.tableLayoutPanel1.SuspendLayout();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.errorIcono)).BeginInit();
@@ -67,10 +69,10 @@
             this.tableLayoutPanel1.Controls.Add(this.txtPrecio, 1, 1);
             this.tableLayoutPanel1.Controls.Add(this.txtCodigo, 1, 3);
             this.tableLayoutPanel1.Controls.Add(this.txtExistencias, 1, 4);
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(11, 54);
-            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(2);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(15, 66);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.Padding = new System.Windows.Forms.Padding(15, 0, 0, 0);
+            this.tableLayoutPanel1.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
             this.tableLayoutPanel1.RowCount = 6;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15F));
@@ -78,17 +80,16 @@
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(532, 255);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(709, 314);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
             // lblNombre
             // 
             this.lblNombre.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblNombre.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.lblNombre.Location = new System.Drawing.Point(17, 0);
-            this.lblNombre.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblNombre.Location = new System.Drawing.Point(23, 0);
             this.lblNombre.Name = "lblNombre";
-            this.lblNombre.Size = new System.Drawing.Size(99, 38);
+            this.lblNombre.Size = new System.Drawing.Size(131, 47);
             this.lblNombre.TabIndex = 0;
             this.lblNombre.Text = "Nombre";
             this.lblNombre.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -99,10 +100,9 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.lblPrecio.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblPrecio.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.lblPrecio.Location = new System.Drawing.Point(17, 38);
-            this.lblPrecio.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblPrecio.Location = new System.Drawing.Point(23, 47);
             this.lblPrecio.Name = "lblPrecio";
-            this.lblPrecio.Size = new System.Drawing.Size(99, 38);
+            this.lblPrecio.Size = new System.Drawing.Size(131, 47);
             this.lblPrecio.TabIndex = 0;
             this.lblPrecio.Text = "Precio";
             this.lblPrecio.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -112,10 +112,9 @@
             this.lblCategoria.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.lblCategoria.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblCategoria.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.lblCategoria.Location = new System.Drawing.Point(17, 76);
-            this.lblCategoria.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblCategoria.Location = new System.Drawing.Point(23, 94);
             this.lblCategoria.Name = "lblCategoria";
-            this.lblCategoria.Size = new System.Drawing.Size(99, 38);
+            this.lblCategoria.Size = new System.Drawing.Size(131, 47);
             this.lblCategoria.TabIndex = 0;
             this.lblCategoria.Text = "Categoria";
             this.lblCategoria.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -125,10 +124,9 @@
             this.lblCodigo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.lblCodigo.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblCodigo.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.lblCodigo.Location = new System.Drawing.Point(17, 114);
-            this.lblCodigo.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblCodigo.Location = new System.Drawing.Point(23, 141);
             this.lblCodigo.Name = "lblCodigo";
-            this.lblCodigo.Size = new System.Drawing.Size(99, 38);
+            this.lblCodigo.Size = new System.Drawing.Size(131, 47);
             this.lblCodigo.TabIndex = 0;
             this.lblCodigo.Text = "Código";
             this.lblCodigo.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -138,10 +136,9 @@
             this.lblExistencias.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.lblExistencias.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblExistencias.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.lblExistencias.Location = new System.Drawing.Point(17, 152);
-            this.lblExistencias.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblExistencias.Location = new System.Drawing.Point(23, 188);
             this.lblExistencias.Name = "lblExistencias";
-            this.lblExistencias.Size = new System.Drawing.Size(99, 38);
+            this.lblExistencias.Size = new System.Drawing.Size(131, 47);
             this.lblExistencias.TabIndex = 0;
             this.lblExistencias.Text = "Existencias";
             this.lblExistencias.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -150,10 +147,10 @@
             // 
             this.cmbCategorias.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cmbCategorias.FormattingEnabled = true;
-            this.cmbCategorias.Location = new System.Drawing.Point(120, 78);
-            this.cmbCategorias.Margin = new System.Windows.Forms.Padding(2);
+            this.cmbCategorias.Location = new System.Drawing.Point(160, 96);
+            this.cmbCategorias.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.cmbCategorias.Name = "cmbCategorias";
-            this.cmbCategorias.Size = new System.Drawing.Size(383, 27);
+            this.cmbCategorias.Size = new System.Drawing.Size(509, 31);
             this.cmbCategorias.TabIndex = 2;
             // 
             // panel1
@@ -161,10 +158,10 @@
             this.panel1.Controls.Add(this.btnCancelar);
             this.panel1.Controls.Add(this.btnGuardar);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel1.Location = new System.Drawing.Point(120, 192);
-            this.panel1.Margin = new System.Windows.Forms.Padding(2);
+            this.panel1.Location = new System.Drawing.Point(160, 237);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(410, 61);
+            this.panel1.Size = new System.Drawing.Size(546, 75);
             this.panel1.TabIndex = 3;
             // 
             // btnCancelar
@@ -176,10 +173,10 @@
             this.btnCancelar.IconChar = FontAwesome.Sharp.IconChar.None;
             this.btnCancelar.IconColor = System.Drawing.Color.Black;
             this.btnCancelar.IconFont = FontAwesome.Sharp.IconFont.Auto;
-            this.btnCancelar.Location = new System.Drawing.Point(276, 11);
-            this.btnCancelar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnCancelar.Location = new System.Drawing.Point(368, 14);
+            this.btnCancelar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnCancelar.Name = "btnCancelar";
-            this.btnCancelar.Size = new System.Drawing.Size(100, 40);
+            this.btnCancelar.Size = new System.Drawing.Size(133, 49);
             this.btnCancelar.TabIndex = 0;
             this.btnCancelar.Text = "Cancelar";
             this.btnCancelar.UseVisualStyleBackColor = false;
@@ -194,10 +191,10 @@
             this.btnGuardar.IconChar = FontAwesome.Sharp.IconChar.None;
             this.btnGuardar.IconColor = System.Drawing.Color.Black;
             this.btnGuardar.IconFont = FontAwesome.Sharp.IconFont.Auto;
-            this.btnGuardar.Location = new System.Drawing.Point(55, 11);
-            this.btnGuardar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnGuardar.Location = new System.Drawing.Point(73, 14);
+            this.btnGuardar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnGuardar.Name = "btnGuardar";
-            this.btnGuardar.Size = new System.Drawing.Size(100, 40);
+            this.btnGuardar.Size = new System.Drawing.Size(133, 49);
             this.btnGuardar.TabIndex = 0;
             this.btnGuardar.Text = "Guardar";
             this.btnGuardar.UseVisualStyleBackColor = false;
@@ -208,10 +205,10 @@
             this.txtNombre.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.txtNombre.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtNombre.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.txtNombre.Location = new System.Drawing.Point(120, 4);
-            this.txtNombre.Margin = new System.Windows.Forms.Padding(2);
+            this.txtNombre.Location = new System.Drawing.Point(160, 6);
+            this.txtNombre.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtNombre.Name = "txtNombre";
-            this.txtNombre.Size = new System.Drawing.Size(383, 29);
+            this.txtNombre.Size = new System.Drawing.Size(509, 34);
             this.txtNombre.TabIndex = 5;
             // 
             // txtPrecio
@@ -219,10 +216,10 @@
             this.txtPrecio.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.txtPrecio.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtPrecio.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.txtPrecio.Location = new System.Drawing.Point(120, 42);
-            this.txtPrecio.Margin = new System.Windows.Forms.Padding(2);
+            this.txtPrecio.Location = new System.Drawing.Point(160, 53);
+            this.txtPrecio.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtPrecio.Name = "txtPrecio";
-            this.txtPrecio.Size = new System.Drawing.Size(383, 29);
+            this.txtPrecio.Size = new System.Drawing.Size(509, 34);
             this.txtPrecio.TabIndex = 6;
             // 
             // txtCodigo
@@ -230,11 +227,11 @@
             this.txtCodigo.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.txtCodigo.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtCodigo.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.txtCodigo.Location = new System.Drawing.Point(120, 118);
-            this.txtCodigo.Margin = new System.Windows.Forms.Padding(2);
+            this.txtCodigo.Location = new System.Drawing.Point(160, 147);
+            this.txtCodigo.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtCodigo.Name = "txtCodigo";
             this.txtCodigo.ReadOnly = true;
-            this.txtCodigo.Size = new System.Drawing.Size(383, 29);
+            this.txtCodigo.Size = new System.Drawing.Size(509, 34);
             this.txtCodigo.TabIndex = 7;
             // 
             // txtExistencias
@@ -242,19 +239,19 @@
             this.txtExistencias.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.txtExistencias.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtExistencias.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.txtExistencias.Location = new System.Drawing.Point(120, 156);
-            this.txtExistencias.Margin = new System.Windows.Forms.Padding(2);
+            this.txtExistencias.Location = new System.Drawing.Point(160, 194);
+            this.txtExistencias.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtExistencias.Name = "txtExistencias";
-            this.txtExistencias.Size = new System.Drawing.Size(383, 29);
+            this.txtExistencias.Size = new System.Drawing.Size(509, 34);
             this.txtExistencias.TabIndex = 8;
             this.txtExistencias.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtExistencias_KeyPress);
             // 
             // txtId
             // 
-            this.txtId.Location = new System.Drawing.Point(21, 313);
-            this.txtId.Margin = new System.Windows.Forms.Padding(2);
+            this.txtId.Location = new System.Drawing.Point(28, 385);
+            this.txtId.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtId.Name = "txtId";
-            this.txtId.Size = new System.Drawing.Size(76, 20);
+            this.txtId.Size = new System.Drawing.Size(100, 22);
             this.txtId.TabIndex = 4;
             this.txtId.Visible = false;
             // 
@@ -265,10 +262,9 @@
             this.lblTitulo.Font = new System.Drawing.Font("Segoe UI Semibold", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblTitulo.ForeColor = System.Drawing.Color.White;
             this.lblTitulo.Location = new System.Drawing.Point(0, 0);
-            this.lblTitulo.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblTitulo.Name = "lblTitulo";
-            this.lblTitulo.Padding = new System.Windows.Forms.Padding(15, 0, 0, 0);
-            this.lblTitulo.Size = new System.Drawing.Size(556, 41);
+            this.lblTitulo.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
+            this.lblTitulo.Size = new System.Drawing.Size(741, 50);
             this.lblTitulo.TabIndex = 2;
             this.lblTitulo.Text = "Registro de Productos";
             this.lblTitulo.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -279,16 +275,31 @@
             this.errorIcono.BlinkStyle = System.Windows.Forms.ErrorBlinkStyle.NeverBlink;
             this.errorIcono.ContainerControl = this;
             // 
+            // cuiFormAnimator1
+            // 
+            this.cuiFormAnimator1.AnimateOnStart = true;
+            this.cuiFormAnimator1.Duration = 400;
+            this.cuiFormAnimator1.EasingType = CuoreUI.Helpers.DrawingHelper.EasingTypes.QuadOut;
+            this.cuiFormAnimator1.StartOpacity = 0D;
+            this.cuiFormAnimator1.TargetForm = this;
+            this.cuiFormAnimator1.TargetOpacity = 1D;
+            // 
+            // cuiFormRounder1
+            // 
+            this.cuiFormRounder1.OutlineColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.cuiFormRounder1.Rounding = 15;
+            this.cuiFormRounder1.TargetForm = this;
+            // 
             // FrmAgregarProductos
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(556, 352);
+            this.ClientSize = new System.Drawing.Size(741, 433);
             this.Controls.Add(this.lblTitulo);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Controls.Add(this.txtId);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "FrmAgregarProductos";
@@ -323,5 +334,7 @@
         private System.Windows.Forms.TextBox txtExistencias;
         private System.Windows.Forms.TextBox txtCodigo;
         private System.Windows.Forms.Label lblNombre;
+        private CuoreUI.Components.cuiFormAnimator cuiFormAnimator1;
+        private CuoreUI.Components.cuiFormRounder cuiFormRounder1;
     }
 }
